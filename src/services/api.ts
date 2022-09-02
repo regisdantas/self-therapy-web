5a5cf24111b6e0227761828374dbed9bf0480401
regisdantas <dantas.regis.m@gmail.com>
2022-09-02 13:48:45 -0300

    Setting default url and port

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
+const server_url = process.env.REACT_APP_SERVER_URL || 'http://localhost';
+const server_port = process.env.REACT_APP_SERVER_PORT || 3333;
+
 export const api = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}:3333`,
+  baseURL: `${server_url}:${server_port}`,
 });
 
 api.interceptors.request.use(async config => {
