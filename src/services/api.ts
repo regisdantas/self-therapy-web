7d880f0be1747f07db324766cc85c0ec7744249d
regisdantas <dantas.regis.m@gmail.com>
2022-09-02 14:19:31 -0300

    No port is used in heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,10 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
-const server_url = process.env.REACT_APP_SERVER_URL || 'http://localhost';
+const server_url = process.env.REACT_APP_SERVER_URL || 'http://localhost:3333';
 
 export const api = axios.create({
-  baseURL:
-    `${server_url}` + (process.env.REACT_APP_SERVER_PORT !== undefined)
-      ? `:${process.env.REACT_APP_SERVER_PORT}`
-      : '',
+  baseURL: server_url,
 });
 
 api.interceptors.request.use(async config => {
